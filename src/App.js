e73e7a6e8dce49b841a1ad75b4107f60db048de9
K Sheenan <kaynesheenan+git@gmail.com>
2022-02-18 19:10:55 +0900

    set up the list and maps basics in app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@
             <CssBaseline /> 
             <Header />
             <Grid container spacing={3} style={{ width: '100%' }}>
-
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
             </Grid>
         </> 
 
