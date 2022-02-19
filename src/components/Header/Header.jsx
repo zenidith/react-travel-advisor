7c78ee8b4ebe0a3db98188f3b2778d3ba848e6cd
K Sheenan <kaynesheenan+git@gmail.com>
2022-02-19 18:34:36 +0900

    fixing up the main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <AppBar position="static">
+            <Toolbar className="classes.toolbar">
+                <Typography variant="h5" className='classes.title'>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className='classes.title'>
+                        Travel Advisor
+                    </Typography>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
