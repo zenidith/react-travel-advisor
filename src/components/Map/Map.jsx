61a331b9eade58fd661f307a908a420b2e9a543d
K Sheenan <kaynesheenan+git@gmail.com>
2022-02-19 20:24:22 +0900

    map still not displaying however the list items are OK

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import googleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlined from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(max-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0};
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <googleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyDuW8ilAul-u9oMcaq8w5IuVqMYUevFns8' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+                >
+            </googleMapReact>
+        </div>
     );
 }
 
