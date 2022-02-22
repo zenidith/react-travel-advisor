c6f312cd99866ca6213e4ff6d452378a2afb1de2
K Sheenan <kaynesheenan+git@gmail.com>
2022-02-22 22:24:54 +0900

    geolocation adjustmnet

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@
  const App = () => { 
     const [places, setPlaces] = useState([]);
     
-    const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0});
+    const [coordinates, setCoordinates] = useState({ });
     const [bounds, setBounds] = useState(null);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(( { coords: {latitude, longitude} } ) => {
-            setCoordinates({})
+            setCoordinates({ lat: latitude, lng: longitude })
         })
     }, []);
 
