b6b9b17a3d5c63e6140dfa6d84c01bcb068e6593
K Sheenan <kaynesheenan+git@gmail.com>
2022-02-25 22:55:25 +0900

    card media @ 1:10:28

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';   
 import LocationIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+
+import useStyles from './styles';
 
 const PlaceDetails = ({ place }) => {
+    const classes = useStyles();
     return (
-        <h1>{place.name}</h1>
-    ); 
+            <Card elevation={6}>
+                <CardMedia />   
+            </Card>
+        ); 
 }
 export default PlaceDetails;
